5987ab918ffa9e9975f39f1d9b1f6a112a3cefa0
Stephen <kerr0273@algonquinlive.com>
2024-09-27 20:36:00 -0400

    fixed road closed image

--- scripts/before-hero.js
+++ scripts/before-hero.js
@@ -19,9 +19,6 @@ $(document).ready(function() {
   var prevIcon = $('<img>').attr({
     'src': 'images/8675309-arrow-left.png'
   });
-  var prevIcon = $('<img>').attr({
-    'alt': 'left arrow'
-  });
   var nextIcon = $('<img>').attr({
     'src': 'images/8675309-arrow-right.png'
   });
